8d5ca04ca0c5e5f381ff26166bac432ca4a2c373
jeblister <jeblister@gmail.com>
2016-07-06 01:47:46 +0000

    resolve margin-collapse issue

--- src/components/app.js
+++ src/components/app.js
@@ -12,25 +12,25 @@ export default class App extends Component {
         </ul>
       </div>
       <div className="container">
-      <div className="sidebar-sidebar">
-        <h4>Pitch Infrastructure</h4>
-        <p>Creative innovator learning curve</p>
-      </div>
-      <div className="sidebar-content">
-        <div>
-          <h5>Prototype rockstar</h5>
-          <p>Churn rate non-disclosure agreement buyer focus ecosystem iPad.Release venture crowdsource burn rate facebook paradigm shift supply chain prototype responsive web design partnership deployment.Innovator crowdsource holy grail advisor.Funding influencer
-            partner network metrics bandwidth leverage traction buzz virality.</p>
-          <p>Twitter stock equity vesting period learning curve launch party pitch innovator series A financing churn rate handshake.Venture graphical user interface bandwidth influencer sales technology pitch.Marketing iPad angel investor.Social media accelerator
-            seed round conversion traction success pitch advisor.</p>
-          <h5>Accelerator partnership</h5>
-          <p>IPhone deployment product management crowdfunding agile development partner network gen-z advisor validation business plan.Founders influencer alpha business model canvas innovator scrum project investor venture ramen bandwidth disruptive.Series
-            A financing partner network direct mailing rockstar seed money market gamification launch party product management virality.Early adopters deployment MVP scrum project supply chain A/B testing handshake.</p>
-          <p>Metrics funding venture pivot non-disclosure agreement client long tail niche market pitch agile development freemium.Mass market incubator user experience iPad scrum project equity burn rate handshake research &amp; development.Gamification infrastructure
-            business-to-business.IPhone branding responsive web design business model canvas buzz founders infrastructure creative stock accelerator funding.</p>
+        <div className="sidebar-sidebar">
+          <h4>Pitch Infrastructure</h4>
+          <p>Creative innovator learning curve</p>
         </div>
-      
-      </div>
+        <div className="sidebar-content">
+          <div>
+            <h5>Prototype rockstar</h5>
+            <p>Churn rate non-disclosure agreement buyer focus ecosystem iPad.Release venture crowdsource burn rate facebook paradigm shift supply chain prototype responsive web design partnership deployment.Innovator crowdsource holy grail advisor.Funding influencer
+              partner network metrics bandwidth leverage traction buzz virality.</p>
+            <p>Twitter stock equity vesting period learning curve launch party pitch innovator series A financing churn rate handshake.Venture graphical user interface bandwidth influencer sales technology pitch.Marketing iPad angel investor.Social media accelerator
+              seed round conversion traction success pitch advisor.</p>
+            <h5>Accelerator partnership</h5>
+            <p>IPhone deployment product management crowdfunding agile development partner network gen-z advisor validation business plan.Founders influencer alpha business model canvas innovator scrum project investor venture ramen bandwidth disruptive.Series
+              A financing partner network direct mailing rockstar seed money market gamification launch party product management virality.Early adopters deployment MVP scrum project supply chain A/B testing handshake.</p>
+            <p>Metrics funding venture pivot non-disclosure agreement client long tail niche market pitch agile development freemium.Mass market incubator user experience iPad scrum project equity burn rate handshake research &amp; development.Gamification infrastructure
+              business-to-business.IPhone branding responsive web design business model canvas buzz founders infrastructure creative stock accelerator funding.</p>
+          </div>
+
+        </div>
       </div>
     </div>);
   }
