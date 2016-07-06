40d7bd72cbe12ac777e63e9ed671570d50e1978c
jeblister <jeblister@gmail.com>
2016-07-06 01:46:56 +0000

    replace container class by root id in index.html

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('#root'));
